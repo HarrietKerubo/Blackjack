4cf557efedf9241eda325091f815c728027f0a8e
Harriet Kerubo <keruboharriet7@gmail.com>
2023-07-20 07:18:11 -0400

    Randomization add

--- index.js
+++ index.js
@@ -1,15 +1,36 @@
-let firstCard = 10;
-let secondCard = 1;
+let firstCard = getRandomCards();
+let secondCard = getRandomCards();
+let cards = [firstCard, secondCard];
 let sum = firstCard + secondCard;
 let hasBlackJack = false;
 let isAlive = true;
 let messageEL = document.getElementById("message-el");
-// let sumEl = document.getElementById("sum-el");
 let sumEl = document.querySelector("#sum-el");
+let cardEl = document.querySelector("#card-el");
 
 let message = "";
 
-function startgame() {
+function getRandomCards() {
+  let randomNumber = Math.floor(Math.random() * 13) + 1;
+  if (randomNumber === 1) {
+    return 11;
+  } else if (randomNumber > 10) {
+    return 10;
+  } else {
+    return randomNumber;
+  }
+}
+
+function startGame() {
+  renderGame();
+}
+
+function renderGame() {
+  cardEl.textContent = "Cards: ";
+  for (let i = 0; i < cards.length; i++) {
+    cardEl.textContent += cards[i] + " ";
+  }
+  sumEl.textContent = "Sum: " + sum;
   if (sum <= 20) {
     message = "Do you want to draw a new card?";
   } else if (sum === 21) {
@@ -19,6 +40,13 @@ function startgame() {
     message = "You're out of the game ";
     isAlive = false;
   }
-  sumEl.textContent = "Sum: " + sum;
+
   messageEL.textContent = message;
+}
+
+function newCard() {
+  let card = getRandomCards();
+  cards.push(card);
+  sum += card;
+  renderGame();
 }
